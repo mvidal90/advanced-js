11c47f6996c404336df188bcc9dcd8756595382f
Marcos Vidal <markos_ari@hotmail.com>
2024-01-25 20:05:59 -0300

    ADD xmlhttprequest examples

---
+++ js/xml-http.js
@@ -0,0 +1,35 @@
+
+// const xhr = new XMLHttpRequest;
+// console.log(xhr.readyState) // 0
+
+// xhr.open("GET", "../data/movies.json");
+// console.log(xhr.readyState) // 1
+
+// xhr.addEventListener("readystatechange", () => {
+//     console.log("EL valor de readystate es: " + xhr.readyState)
+//     if (xhr.readyState === 4) {
+//         console.log(JSON.parse(xhr.response))
+//     }
+// })
+
+// xhr.send()
+
+// console.log(xhr)
+
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+    const xhr = new XMLHttpRequest;
+    xhr.open("GET", "../templates/more-info.html");
+
+    xhr.addEventListener("readystatechange", () => {
+        if (xhr.readyState === 4) {
+            const mainElement = document.querySelector("main");
+            const div = document.createElement("div")
+            div.innerHTML = xhr.response
+            mainElement.appendChild(div)
+        }
+    })
+    
+    xhr.send()
+})
